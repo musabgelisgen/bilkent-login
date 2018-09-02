bcdc7800ef94c66d34cc1eb02b8f5c1b5b7eb428
musabgelisgen <mgelisgen97@gmail.com>
2018-09-02 16:25:28 +0300

    initial commit

---
+++ content.js
@@ -0,0 +1,26 @@
+(function(){
+	var id = "<id_here>";
+	var srs_password = "<srs_password_here>";
+	var email_address = "<email_addess_here>";
+	var email_password = "<email_password_here>";
+
+	var url = window.location.href;
+
+	if (url.includes("webmail")){
+		var not_loginned_yet = (document.getElementById("rcmloginuser") != null && document.querySelector('[id^="LoginForm-"]') != null);
+
+		if (not_loginned_yet){
+			document.getElementById("rcmloginuser").value = email_address;
+			document.querySelector('[id^="LoginForm-"]').value = Array(email_password.length).join('*');
+			document.getElementById("LoginForm_password").value = email_password;
+			document.getElementById("rcm-login-form").submit();	
+		}
+	}
+	else{
+		document.getElementById("LoginForm_username").value = id;
+		document.querySelector('[id^="LoginForm-"]').value = Array(srs_password.length).join('*');
+		document.getElementById("LoginForm_password").value = srs_password;
+		document.getElementById("login-form").submit();	
+	}
+	
+})();
